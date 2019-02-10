bda68bf379d444ea93a8161639812fc5d0225c47
webnetkz <8gg001@gmail.com>
2019-02-10 19:40:03 +0600

    start project PHPini

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>PHPini</title>
+
+        <meta charset="UTF-8">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="miniLogoWebnet.png" type="image/png">
+        <link rel="stylesheet" href="public/css/style.css">
+          
+    </head>
+
+    <body>
+        <div id="controlPanel">
+
+        </div>
+
+        <div id="description">
+
+        </div>
+
+    </body>
+</html>
